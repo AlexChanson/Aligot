f543d4ac583eef1957da11827a6b36944a35d316
Blackcraps <leo.brunot@outlook.fr>
2017-03-18 15:10:53 +0100

    test

--- src/physics/RigidBody.java
+++ src/physics/RigidBody.java
@@ -12,8 +12,17 @@ public class RigidBody {
     double masse;
     double friction;
 
-    public RigidBody(Vector2D position, double size, double size){
+    public RigidBody(Vector2D position, double size, double masse){
+        this.position = position;
+        this.size = size;
+        this.masse = masse;
+        velocity = new Vector2D(0, 0);
+        acceleration = new Vector2D(0 , 0);
+        friction = 1;
+    }
 
+    public void updatePosition(double dt){
+        position = position + velocity.multiply(dt);
     }
 
 }
