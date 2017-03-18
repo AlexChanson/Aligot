460fc3a1dada794be0e8a676125286eda2657af3
Blackcraps <leo.brunot@outlook.fr>
2017-03-18 14:51:59 +0100

    test

--- src/physics/RigidBody.java
+++ src/physics/RigidBody.java
@@ -4,4 +4,16 @@ package physics;
  * Created by ben on 18/03/17.
  */
 public class RigidBody {
+
+    Vector2D position;
+    Vector2D velocity;
+    Vector2D acceleration;
+    double size;
+    double masse;
+    double friction;
+
+    public RigidBody(Vector2D position, double size, double size){
+
+    }
+
 }
