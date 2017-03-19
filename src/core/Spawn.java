fd31ad1be736a7fe33588a17a782fd3ff58575bd
Alexandre Chanson <a.chanson.tours@gmail.com>
2017-03-19 16:09:04 +0100

    working on data structure

--- src/core/Spawn.java
+++ src/core/Spawn.java
@@ -1,4 +1,15 @@
 package core;
 
+import physics.Vector2D;
+
 public class Spawn {
+    private Vector2D position;
+
+    public Spawn(Vector2D position) {
+        this.position = position;
+    }
+
+    public Vector2D getPosition() {
+        return position;
+    }
 }
