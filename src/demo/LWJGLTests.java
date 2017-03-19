c1f72a5b9f5db770ed6cd77514de8e0a8c2db014
Alexandre Chanson <a.chanson.tours@gmail.com>
2017-03-19 16:59:58 +0100

    Epilepsy mode off.

--- src/demo/LWJGLTests.java
+++ src/demo/LWJGLTests.java
@@ -13,7 +13,7 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
-public class Main {
+public class LWJGLTests {
 
     // The window handle
     private long window;
@@ -48,7 +48,7 @@ public class Main {
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
 
         // Create the window
-        window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+        window = glfwCreateWindow(1280, 720, "Vive l'Aligot", NULL, NULL);
         if ( window == NULL )
             throw new RuntimeException("Failed to create the GLFW window");
 
@@ -94,11 +94,12 @@ public class Main {
         // bindings available for use.
         GL.createCapabilities();
 
+        // Set the clear color
+        glClearColor((float) Math.random(), (float) Math.random(), (float) Math.random(), 1.0f);
+
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
         while ( !glfwWindowShouldClose(window) ) {
-            // Set the clear color
-            glClearColor((float) Math.random(), (float) Math.random(), (float) Math.random(), (float) Math.random());
 
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
 
@@ -111,7 +112,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        new Main().run();
+        new LWJGLTests().run();
     }
 
 }
