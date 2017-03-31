9d6555eb31d9c57c70e7140040dd0eebc2e2ca90
KasakiGogensu <Kasakigogensu@live.fr>
2017-03-31 10:15:25 +0200

    Add files via upload
    Some documentation

--- src/graphics/Window.java
+++ src/graphics/Window.java
@@ -3,6 +3,9 @@ package graphics;
 import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
 
+import java.awt.*;
+import java.nio.IntBuffer;
+
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 
@@ -30,13 +33,11 @@ public class Window {
     public static void drawTexture(Texture texture, int posX, int posY, float rotate, float scale) {
         float relWidth = texture.getWidth() / 2, relHeight = texture.getHeight() / 2;
 
-        glEnable(GL_TEXTURE_2D);
         glPushMatrix();
 
         glTranslatef(posX, posY, 0);
         glRotatef(rotate, 0, 0f, 1f);
         glScalef(scale, scale, 1);
-        glColor3f(1, 1 ,1);
 
         glBindTexture(GL_TEXTURE_2D, texture.getId());
 
@@ -52,78 +53,14 @@ public class Window {
         glEnd();
 
         glPopMatrix();
-        glDisable(GL_TEXTURE_2D);
     }
 
     public static void drawSprite(String fileName, int posX, int posY, float rotate, float scale) {
-        String path = System.getProperty("user.dir") + "/ressources/sprites/" + fileName;
+        String path = System.getProperty("user.dir") + "/sprites/" + fileName;
 
-        SpriteTexture texture = new SpriteTexture(path);
+        Texture texture = new Texture(path);
 
         drawTexture(texture, posX, posY, rotate, scale);
-    }
-
-    public static void drawLine(int x1, int y1, int x2, int y2, float thickness, int R, int G, int B){
-        int minX, minY;
-
-        minX = Math.min(x1, x2);
-        minY = Math.min(y1, y2);
-
-        x1 -= minX;
-        x2 -= minX;
-        y1 -= minY;
-        y2 -= minY;
-
-        glPushMatrix();
-
-        glBindTexture(0, 0);
-        glTranslatef(minX, minY, 0);
-        glColor3f(((float) R) / 255.0f, ((float) G) / 255.0f, ((float) B) / 255.0f);
-        glLineWidth(thickness);
-
-        glBegin(GL_LINES);
-            glVertex2f(x1, y1);
-            glVertex2f(x2, y2);
-        glEnd();
-
-        glPopMatrix();
-    }
-
-    public static void drawRectangle(int width, int height, int posX, int posY, int R, int G, int B, float rotate){
-        glPushMatrix();
-
-        glBindTexture(0, 0);
-        glTranslatef(posX, posY, 0);
-        glColor3f(((float) R) / 255.0f, ((float) G) / 255.0f, ((float) B) / 255.0f);
-
-        glBegin(GL_QUADS);
-            glVertex2f(-width, -height);
-            glVertex2f(-width, height);
-            glVertex2f(width, height);
-            glVertex2f(width, -height);
-        glEnd();
-
-        glPopMatrix();
-    }
-
-    public static void drawCircle(int posX, int posY, int radius, int R, int G, int B) {
-        double a = 1.0 / radius;
-
-        glPushMatrix();
-
-        glBindTexture(0, 0);
-        glTranslatef(posX, posY, 0);
-        glColor3f(((float) R) / 255.0f, ((float) G) / 255.0f, ((float) B) / 255.0f);
-
-        glBegin(GL_POLYGON);
-
-        for (double i = -Math.PI; i < Math.PI; i += a) {
-            glVertex2f((float) (Math.cos(i) * radius), (float) (Math.sin(i) * radius));
-        }
-
-        glEnd();
-
-        glPopMatrix();
     }
 
     public static boolean shouldClose() {
@@ -148,14 +85,5 @@ public class Window {
 
     private static long getMonitor() {
         return glfwGetMonitors().get(0);
-    }
-
-    public static long getWindow() {
-        return window;
-    }
-
-    public static void exit(){
-        glfwDestroyWindow(window);
-        glfwTerminate();
     }
 }
