4948112cccdc9d14b9b1d13a4579f16f4f862863
Christopher <kasakigogensu@live.fr>
2017-04-10 14:26:40 +0200

    Button creation

---
+++ src/graphics/gui/Button.java
@@ -0,0 +1,24 @@
+package graphics.gui;
+
+import graphics.Texture;
+
+/**
+ * Created by Christopher on 10/04/2017.
+ */
+public class Button extends GUIComponent {
+    private Texture texture;
+
+    public Button (int x, int y, Texture texture, int width, int height){
+        this.x = x;
+        this.y = y;
+        this.texture = texture;
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void render() throws Exception {
+
+    }
+
+}
