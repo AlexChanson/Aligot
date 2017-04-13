1ef8a1d5338d3e498996fdd45ef7aba496c68860
Alexandre Chanson <a.chanson.tours@gmail.com>
2017-04-13 16:18:47 +0200

    small fix

--- src/core/Magazine.java
+++ src/core/Magazine.java
@@ -3,14 +3,29 @@ package core;
 
 public class Magazine extends Item {
     private Ammunition ammo;
+    private int bullets, maxBullets;
 
-    public Magazine(String name, String icon, Ammunition ammo) {
+    public Magazine(String name, String icon, Ammunition ammo, int capacity) {
         super(name, icon);
         this.ammo = ammo;
+        bullets = capacity;
+        maxBullets = capacity;
     }
 
     @Override
     public void use(Player user) {
 
+    }
+
+    public Ammunition getAmmo() {
+        return ammo;
+    }
+
+    public int getBullets() {
+        return bullets;
+    }
+
+    public int getMaxBullets() {
+        return maxBullets;
     }
 }
--- src/graphics/Colorizer.java
+++ src/graphics/Colorizer.java
@@ -6,20 +6,34 @@ import java.awt.image.BufferedImage;
 
 public class Colorizer {
 
-    public static BufferedImage colorize(BufferedImage original, Color tint) {
+    public static BufferedImage colorize(BufferedImage original, Color tint, int seuil) {
         BufferedImage img = new BufferedImage(original.getWidth(), original.getHeight(),BufferedImage.TYPE_4BYTE_ABGR);
 
         for (int x = 0; x < original.getWidth(); ++x) {
             for (int y = 0; y < original.getHeight(); ++y) {
                 Color pixelVal = new Color(original.getRGB(x, y), true);
-                int grayValue = pixelVal.getRed();
-                int newRed = (tint.getRed() * grayValue) / 255;
-                int newGreen = (tint.getGreen() * grayValue) / 255;
-                int newBlue = (tint.getBlue() * grayValue) / 255;
+                int grayValue = (pixelVal.getBlue()+pixelVal.getGreen()+pixelVal.getRed())/3;
+                int newRed;
+                int newGreen;
+                int newBlue;
+                if(grayValue > seuil){
+                    newRed = grayValue;
+                    newGreen = grayValue;
+                    newBlue = grayValue;
+                }else{
+                    newRed = (tint.getRed()/255) * grayValue;
+                    newGreen = (tint.getGreen()/255) * grayValue;
+                    newBlue = (tint.getBlue()/255) * grayValue;
+                }
+
                 img.setRGB(x, y, new Color(newRed, newGreen, newBlue, pixelVal.getAlpha()).getRGB());
             }
         }
         return img;
+    }
+
+    public static BufferedImage colorize(BufferedImage original, Color tint){
+        return colorize(original, tint, 200);
     }
 
 }
