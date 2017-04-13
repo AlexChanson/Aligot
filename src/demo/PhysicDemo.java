f7266ed42de4b39551f6ee082b03e433aee90099
ben <bencrulis@gmail.com>
2017-04-13 19:06:21 +0200

    Added restitution within collision and all its magnificience!

--- src/demo/PhysicDemo.java
+++ src/demo/PhysicDemo.java
@@ -16,11 +16,16 @@ public class PhysicDemo {
         sim.addSolver(new NewtonGravitationSolver(6.67e-11));
         sim.addSolver(new AirDampingSolver(0.01, 0.02));
         sim.addSolver(new CollisionSolver());
-        RigidBody body1 = new RigidBody(new Vector2D(400,400), 140, 83.6);
-        RigidBody body2 = new RigidBody(new Vector2D(800,400), 70, 40);
+        RigidBody body1 = new RigidBody(new Vector2D(400,440), 10, 40);
+        RigidBody body2 = new RigidBody(new Vector2D(800,400), 200, 2e17);
+        body1.setRestitution(0.95);
+        body2.setRestitution(0.95);
+        body1.setFriction(0.2);
+        body2.setFriction(0.2);
+        body2.setAttractive(true);
 
-        body1.setVelocity(new Vector2D(100,0));
-        body2.setVelocity(new Vector2D(-50,0));
+        body1.setVelocity(new Vector2D(200,200));
+        body2.setVelocity(new Vector2D(0,0));
 
 
         sim.addBody(body1);
@@ -40,11 +45,14 @@ public class PhysicDemo {
             Window.drawCircle((int)body2.getPosition().getX(), (int)body2.getPosition().getY(), (int)body2.getRadius(),255, 0, 0);
             Window.drawCircle((int)body1.getPosition().getX(), (int)body1.getPosition().getY(), (int)body1.getRadius(),0, 255, 0);
             Vector2D pos = body1.getPosition();
-            int forceX = (int) ((pos.getX()+body1.getAcceleration().getX()*0.3));
-            int forceY = (int) ((pos.getY()+body1.getAcceleration().getY()*0.3));
+            int forceX = (int) ((pos.getX()+body1.getAcceleration().getX()*0.03));
+            int forceY = (int) ((pos.getY()+body1.getAcceleration().getY()*0.03));
 
-            Window.drawLine((int)pos.getX(), (int)pos.getY(), forceX, forceY, 10,0, 255,0);
-
+            Window.drawLine((int)pos.getX(), (int)pos.getY(), forceX, forceY, 3,255, 0,0);
+            Window.drawLine((int)pos.getX(), (int)pos.getY(), (int) (pos.getX()+body1.getAcceleration().getX()/30),
+                    (int) (pos.getY()+body1.getAcceleration().getY()/30), 3,255, 255,0);
+            Window.drawLine((int)pos.getX(), (int)pos.getY(), (int) (pos.getX()+body1.getVelocity().getX()/5),
+                    (int) (pos.getY()+body1.getVelocity().getY()/5), 3,0, 255,0);
             Window.swapBuffers();
             glfwPollEvents();
 
