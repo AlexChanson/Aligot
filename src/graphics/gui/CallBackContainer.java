61c3550d981dd83ea20b8a0d97e87504c58e9d04
Alexandre Chanson <a.chanson.tours@gmail.com>
2017-04-18 17:44:18 +0200

    changed implementation to a less weird one

--- src/graphics/gui/CallBackContainer.java
+++ src/graphics/gui/CallBackContainer.java
@@ -1,26 +1,15 @@
 package graphics.gui;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 /**
  * @author Alexandre
- * A weird thing I made to store the callback code for the interface Buttons
  */
 public class CallBackContainer {
     private GUIComponent guiComponent;
     private GUIListener guiListener;
-    private Method run;
 
     public CallBackContainer(GUIComponent guiComponent, GUIListener listener) {
         this.guiComponent = guiComponent;
         this.guiListener = listener;
-        try {
-            this.run = listener.getClass().getMethod("execute");
-            this.run.setAccessible(true);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        }
     }
 
     public GUIComponent getGuiComponent() {
@@ -28,10 +17,6 @@ public class CallBackContainer {
     }
 
     public void execute() {
-        try {
-            run.invoke(guiListener);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
-        }
+        guiListener.execute();
     }
 }
