775e02d898ab5546d239f1807a5603e08571d0ce
Alexandre Chanson <a.chanson.tours@gmail.com>
2017-04-19 17:42:08 +0200

    added a function to init the engine latter

--- src/gamelauncher/Game.java
+++ src/gamelauncher/Game.java
@@ -20,6 +20,7 @@ public class Game implements GameStart {
     private static Engine engine;
     private static GraphicsEngine graphicsEngine;
     private static Level currentLevel;
+    private static Player p1, p2;
 
     @Override
     public void start(int screenHeight, int screenWidth, boolean fullscreen, String firstPlayerName, String secondPlayerName) {
@@ -39,10 +40,10 @@ public class Game implements GameStart {
         LevelGen gen = new LevelGen(new Random().nextLong(), mapSize);
         currentLevel = gen.getLevel();
 
-        Player p1 = new Player(new RigidBody(new Vector2D(100,100),2, 70), "doomguy.jpg", firstPlayerName, 100);
-        Player p2 = new Player(new RigidBody(new Vector2D(screenWidth-100,screenHeight-100),2, 70), "doomguy.jpg", secondPlayerName, 100);
+        p1 = new Player(new RigidBody(new Vector2D(100,100),2, 70), "doomguy.jpg", firstPlayerName, 100);
+        p2 = new Player(new RigidBody(new Vector2D(screenWidth-100,screenHeight-100),2, 70), "doomguy.jpg", secondPlayerName, 100);
 
-        engine = new Engine(currentLevel, p1, p2);
+        initEngine();
         GraphicsEngine graphicsEngine = new GraphicsEngine();
 
         FiniteStateMachine myMachine = new FiniteStateMachine();
@@ -56,10 +57,10 @@ public class Game implements GameStart {
             Window.loopStart();
             myMachine.update();
 
-            engine.update();
-            if(currentLevel != null){
+            if(engine != null)
+                engine.update();
+            if(currentLevel != null)
                 graphicsEngine.drawLevel(currentLevel);
-            }
             graphicsEngine.drawGui();
 
             Window.loopEnd();
@@ -79,5 +80,20 @@ public class Game implements GameStart {
 
     public static void setLevel(Level level){
         currentLevel = level;
+        //TODO reset players properly
+        p1.setHealth(p1.getMaxHealth());
+        p2.setHealth(p2.getMaxHealth());
+        engine = null;
+        initEngine();
+    }
+
+    private static void initEngine(){
+        if(engine != null){
+            try {
+                engine = new Engine(currentLevel, p1, p2);
+            }catch (NullPointerException e){
+                System.out.println("Engine Init Failed");
+            }
+        }
     }
 }
