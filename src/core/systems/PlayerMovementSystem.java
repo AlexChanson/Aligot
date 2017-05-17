60317c6acbea9301c37a097ff98e3b49febb01e8
ben <bencrulis@gmail.com>
2017-05-17 17:30:15 +0200

    player movement is now smooth

--- src/core/systems/PlayerMovementSystem.java
+++ src/core/systems/PlayerMovementSystem.java
@@ -10,11 +10,12 @@ import java.util.logging.Logger;
 
 public class PlayerMovementSystem extends SubSystem {
     private static final Logger LOGGER = Logger.getLogger( PlayerMovementSystem.class.getName() );
-    int[] dir = {0,0};
+    HashMap<Player, Integer> playerMovements;
 
     @Override
     public void initialize() {
         LOGGER.info("Init OK");
+        playerMovements = new HashMap<>();
     }
 
     @Override
@@ -23,25 +24,24 @@ public class PlayerMovementSystem extends SubSystem {
         switch (event.type) {
             case "PLAYER_MOVEMENT_STARTED": {
                 int direction = (int) event.data;
-                dir[0] = 1;
-                dir[1] = direction;
+                playerMovements.put(player, direction);
                 break;
             }
             case "PLAYER_MOVEMENT_ENDED": {
                 int direction = (int) event.data;
-                dir[0] = 0;
+                playerMovements.put(player, -1);
                 break;
             }
             case "PLAYER_CHANGED":
-                dir[0] = 0;
                 break;
             case "TICK":
-                if (dir[0] == 1) {
+                if (playerMovements.containsKey(player) ) {
+                    int direction = playerMovements.get(player);
                     double orientation = player.getRotation();
-                    if (dir[1] == 0) {
+                    if (direction == 0) {
                         orientation += Math.PI / 2;
                         player.setLooking_right(true);
-                    } else {
+                    } else if (direction == 1) {
                         player.setLooking_right(false);
                         orientation -= Math.PI / 2;
                     }
