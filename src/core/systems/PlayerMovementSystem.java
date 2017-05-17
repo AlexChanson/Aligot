9af87e8224e070844fbf8ea3fd3649768d74b4a5
Alexandre Chanson <a.chanson.tours@gmail.com>
2017-05-17 15:38:18 +0200

    changed the way player movement is handled

--- src/core/systems/PlayerMovementSystem.java
+++ src/core/systems/PlayerMovementSystem.java
@@ -5,10 +5,12 @@ import core.Player;
 import physics.RigidBody;
 import physics.Vector2D;
 
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 public class PlayerMovementSystem extends SubSystem {
     private static final Logger LOGGER = Logger.getLogger( PlayerMovementSystem.class.getName() );
+    int[] dir = {0,0};
 
     @Override
     public void initialize() {
@@ -18,26 +20,38 @@ public class PlayerMovementSystem extends SubSystem {
     @Override
     protected void processEvent(Event event) {
         Player player = engine.getActivePlayer();
-        if (event.type.equals("PLAYER_MOVEMENT")){
-            int direction = (int) event.data;
-
-            double orientation = player.getRotation();
-            if ( direction == 0 ){
-                orientation += Math.PI/2;
-                player.setLooking_right(true);
+        switch (event.type) {
+            case "PLAYER_MOVEMENT_STARTED": {
+                int direction = (int) event.data;
+                dir[0] = 1;
+                dir[1] = direction;
+                break;
             }
-            else {
-                player.setLooking_right(false);
-                orientation -= Math.PI/2;
+            case "PLAYER_MOVEMENT_ENDED": {
+                int direction = (int) event.data;
+                dir[0] = 0;
+                break;
             }
-
-            RigidBody body = player.getRigidBody();
-            if ( player.isOn_ground() ){
-                body.setVelocity(Vector2D.createFromAngle(40, orientation));
-            }
-        }
-        if ( event.type.equals("TICK") ){
-            player.setOn_ground(false);
+            case "PLAYER_CHANGED":
+                dir[0] = 0;
+                break;
+            case "TICK":
+                if (dir[0] == 1) {
+                    double orientation = player.getRotation();
+                    if (dir[1] == 0) {
+                        orientation += Math.PI / 2;
+                        player.setLooking_right(true);
+                    } else {
+                        player.setLooking_right(false);
+                        orientation -= Math.PI / 2;
+                    }
+                    RigidBody body = player.getRigidBody();
+                    if (player.isOn_ground()) {
+                        body.setVelocity(Vector2D.createFromAngle(40, orientation));
+                    }
+                }
+                player.setOn_ground(false);
+                break;
         }
     }
 }
