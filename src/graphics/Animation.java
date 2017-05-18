6638524bfd5ad5c7c9953d1cca34738135d5bebe
Clément <3065167a@opayq.com>
2017-05-18 22:05:02 +0200

    Amélioration d'Animation

--- src/graphics/Animation.java
+++ src/graphics/Animation.java
@@ -1,12 +1,17 @@
 package graphics;
 
 import com.google.gson.annotations.Expose;
+
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class Animation {
+    private static String ressourcesFolderPath = System.getProperty("user.dir") + File.separator + "ressources" + File.separator + "assets" + File.separator;
     private List<String> sprites;
     private List<Double> durations;
+    private ArrayList<Texture> textures = new ArrayList<>();
 
     @Expose(serialize = false, deserialize = false)
     private double time = 0;
@@ -68,20 +73,28 @@ public class Animation {
         this.time = 0;
     }
 
-    public void draw(int posX, int posY, int width, int height, float rotate) {
+    public void draw(int posX, int posY, int width, int height, float rotate, float scale) {
+        if (this.textures.size() == 0) {
+            Iterator<String> it_sprt = this.sprites.iterator();
+
+            while (it_sprt.hasNext()) {
+                this.textures.add(new Texture(ressourcesFolderPath + it_sprt.next()));
+            }
+        }
+
         Iterator<Double> it_dur = this.durations.iterator();
-        Iterator<String> it_sprt = this.sprites.iterator();
+        Iterator<Texture> it_text = this.textures.iterator();
+        Texture text;
         double t = 0;
-        String sprt;
         boolean cont = true;
 
-        while (it_dur.hasNext() && it_sprt.hasNext() && cont) {
+        while (it_dur.hasNext() && it_text.hasNext() && cont) {
             t += it_dur.next();
-            sprt = it_sprt.next();
+            text = it_text.next();
 
             if (t > this.time) {
                 cont = false;
-                Window.drawSprite(sprt, posX, posY, width, height, rotate);
+                Window.drawTexture(text, posX, posY, width, height, rotate, scale, 0, 0, text.getWidth(), text.getHeight(), 255, 255, 255);
             }
         }
     }
