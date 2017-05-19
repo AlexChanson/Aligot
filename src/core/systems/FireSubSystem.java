d3a18324b004fc5a98ba84281ff3270a014d3ba5
Alexandre Chanson <a.chanson.tours@gmail.com>
2017-05-19 16:19:36 +0200

    fixed angle

--- src/core/systems/FireSubSystem.java
+++ src/core/systems/FireSubSystem.java
@@ -22,13 +22,13 @@ public class FireSubSystem extends SubSystem{
         if(event.type.equals("FIRE")){
             Player shooter = engine.getActivePlayer();
             if(shooter.getCurrentWeapon() != null){
-                Vector2D firingAngle = Vector2D.createFromAngleDegree(1, shooter.getGlobalWeaponOrientation() + 90);
+                Vector2D firingAngle = Vector2D.createFromAngleDegree(1, shooter.getGlobalWeaponOrientation() - 90);
                 Vector2D projectileOrigin = new Vector2D(shooter.getRigidBody().getPosition());
                 projectileOrigin = projectileOrigin.add(firingAngle.multiply(15.5));
                 RigidBody b = new RigidBody(projectileOrigin, 5, 50);
                 b.setStaticObject(false);
                 b.setAttractive(true);
-                b.setVelocity(projectileOrigin.multiply(10));
+                b.setVelocity(firingAngle.multiply(200));
                 Projectile projectile = new Projectile(b, "star.png", shooter.getCurrentWeapon().getAmmo(), shooter.getCurrentWeapon(), shooter);
                 engine.getProjectiles().add(projectile);
                 engine.getPhysicsEngine().addBody(b);
