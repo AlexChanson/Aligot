ba996cbe9ec0fd22a4a553e3a7a4b8e6712cdd65
Alexandre Chanson <a.chanson.tours@gmail.com>
2017-05-21 12:27:14 +0200

    working on progression save

--- src/core/model/Progression.java
+++ src/core/model/Progression.java
@@ -1,7 +1,47 @@
 package core.model;
 
+import com.google.gson.Gson;
+import utility.Loader;
+import utility.Pair;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
 public class Progression {
+    private HashMap<Pair<Level, Player>, Integer> progression = new HashMap<>();
 
-    //TODO Save player progression through challenges
+    public Progression(HashMap<Pair<Level, Player>, Integer> progression) {
+        this.progression = progression;
+    }
 
+    public static Progression load(){
+        File f = new File(Loader.getSpriteFolderPath() + "progression.dat");
+        if (!f.exists()){
+            return new Progression(new HashMap<>());
+        }else {
+            try {
+                String content = new String(Files.readAllBytes(Paths.get("duke.java")));
+                Gson gson = new Gson();
+                return gson.fromJson(content, Progression.class);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return new Progression(new HashMap<>());
+            }
+        }
+    }
+
+    public static void save(Progression p){
+        File f = new File(Loader.getSpriteFolderPath() + "progression.dat");
+        if (!f.exists()){
+            try {
+                f.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
